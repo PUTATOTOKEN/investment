0340124df71c8aafed82b4d4e81791e511f4b6b4
PUTATOTOKEN <putato_token@hotmail.com>
2025-05-24 20:45:10 +0330

    Add files via upload

---
+++ src/pages/register.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import SignUp from '../components/auth/SignUp';
+
+const Register = () => {
+  return (
+    <div className="min-h-screen bg-gray-100 flex items-center justify-center">
+      <SignUp />
+    </div>
+  );
+};
+
+export default Register;
