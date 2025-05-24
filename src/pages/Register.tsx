89fe569a689dda92d657a39ca74a175ebb484362
PUTATOTOKEN <putato_token@hotmail.com>
2025-05-24 21:18:26 +0330

    Update and rename register.tsx to Register.tsx

--- src/pages/Register.tsx
+++ src/pages/Register.tsx
@@ -1,13 +1,12 @@
-// src/pages/Register.tsx
-import React from 'react'
-import SignUp from '../components/auth/SignUp'
+import React from 'react';
+import SignUp from '../components/auth/SignUp';
 
 const Register = () => {
   return (
     <div className="min-h-screen bg-gray-100 flex items-center justify-center">
       <SignUp />
     </div>
-  )
-}
+  );
+};
 
-export default Register
+export default Register;
